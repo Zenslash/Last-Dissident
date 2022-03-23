91425d5da910643b9010f29ff02df973766f9c72
Artem <34547317+Zenslash@users.noreply.github.com>
2022-03-23 16:39:32 +0300

    Loot System

--- Cyberpunk2022/Intermediate/Build/Win64/UE4Editor/Inc/Cyberpunk2022/Cyberpunk2022.init.gen.cpp
+++ Cyberpunk2022/Intermediate/Build/Win64/UE4Editor/Inc/Cyberpunk2022/Cyberpunk2022.init.gen.cpp
@@ -25,8 +25,8 @@ void EmptyLinkFunctionForGeneratedCodeCyberpunk2022_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x66E7D94C,
-				0xCF24F344,
+				0xD8CE2B97,
+				0x393C5699,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
