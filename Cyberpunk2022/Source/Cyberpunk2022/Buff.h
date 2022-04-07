af7e4702f3eb175b059a3a73455b6449e2f67991
Artem <34547317+Zenslash@users.noreply.github.com>
2022-04-07 19:47:12 +0300

    Pickups v2(W.I.P)

---
+++ Cyberpunk2022/Source/Cyberpunk2022/Buff.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Buff.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNK2022_API UBuff : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	//void SetBuffParam()
+
+	//void ApplyBuff()
+
+private:
+
+	//int& _buffParam;
+	
+};
---
+++ Cyberpunk2022/Source/Cyberpunk2022/BuffFactory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BuffFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNK2022_API UBuffFactory : public UObject
+{
+	GENERATED_BODY()
+	
+};
